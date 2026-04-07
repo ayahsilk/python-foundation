a70b7dad16521841e49d6aed54cb4035940193a9
ayahhhh <10100@isava.org>
2026-04-07 20:42:47 +0000

    commit

--- 03_calculator.py
+++ 03_calculator.py
@@ -3,4 +3,7 @@ num1=int(input("enter first number:"))
 num2=int(input('enter second number:"'))
 result=num1+num2
 diff=num1-num2
+multi=num1*num2
 print(f"the total of {num1}and{num2}is:{result}")
+print(diff)
+print(multi)
