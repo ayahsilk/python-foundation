f8b9419840dec3cd8e3a9457119d421da9f5f9ad
ayahhhh <10100@isava.org>
2026-04-07 19:45:35 +0000

    commit

--- 02_storygenerater.py
+++ 02_storygenerater.py
@@ -4,3 +4,7 @@ action= input("enter an action (verb ending in -ing):")
 place= input("enter a place:")
 feeling= input("how are you feeling today?")
 adj= input("fighting in a battle to save ninjago")
+#creating the story
+story=f"one day,{name}was{action}at the{place}. it made them feel very{feeling}!"
+print("\n--YOUR ADVENTURE STORY---")
+print(story)
