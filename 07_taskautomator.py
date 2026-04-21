baa6de729896df2b7465bb38fc6279964b856925
ayahhhh <10100@isava.org>
2026-04-21 20:37:01 +0000

    cmmit

--- 07_taskautomator.py
+++ 07_taskautomator.py
@@ -2,4 +2,6 @@
 #range (5) means it will run 5 times (0,1,2,3,4)
 ***for loops with range:** controlling the exact number of times a block of code repeats. 
 *** string accumulation:** using a string variable to "collect" data from inside a loops.
-###**
+###**practical applications**
+1. **rocket launcher ('06_while_countdown.py'):**managed a numeric state until a condition was met.
+**rocket launcher('07_for_repetition.py'):**used 'range()' to generate a specif
