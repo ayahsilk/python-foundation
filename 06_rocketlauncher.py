1e1b27001b86b103071af0c400f2c633dfd6244c
ayahhhh <10100@isava.org>
2026-04-21 20:03:56 +0000

    commit

---
+++ 06_rocketlauncher.py
@@ -0,0 +1,11 @@
+#---STEP 1: INITIALIZE---
+timer=10
+#---STEP 2: THE LOOP---
+while timer>0:
+    print(f"T-Minus{timer}...")
+    timer-=1 #CRITICAL: This reduces the timer by 1 each time
+    #---STEP 3: THE FINISH---
+    print("LIFTOFF!")
+    #---STEP 1: DEFINE THE RANGE---
+    #range(5) means it will run 5 times (0,1,2,3,4)
+    
