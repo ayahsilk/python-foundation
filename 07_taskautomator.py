71c858fb5c3c0205c69590a2dd247ff040eb6ce2
ayahhhh <10100@isava.org>
2026-04-21 20:15:05 +0000

    done

---
+++ 07_taskautomator.py
@@ -0,0 +1,5 @@
+#---STEP 1: DEFINE THE RANGE---
+#range (5) means it will run 5 times (0,1,2,3,4)
+***for loops with range:** controlling the exact number of times a block of code repeats. 
+*** string accumulation:** using a string variable to "collect" data from inside a loops.
+###**
