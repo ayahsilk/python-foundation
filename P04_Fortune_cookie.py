d2d2485cb8d0f6a8181772f4fc060753e2964024
ayahhhh <10100@isava.org>
2026-04-21 19:48:38 +0000

    done

--- P04_Fortune_cookie.py
+++ P04_Fortune_cookie.py
@@ -1,24 +1,26 @@
+import random
+
 fortune_number=random.randint(1,8)
 lucky_number=random.randint(1,100)
 fortune_text=""
 if fortune_number==1:
-fortune_text= "a soft voice may be awfully persuasive."
-elif fortune_number==2
-fortune_text= "you will soon discover a hidden talent."
+    fortune_text= "a soft voice may be awfully persuasive."
+elif fortune_number==2:
+    fortune_text= "you will soon discover a hidden talent."
 elif fortune_number==3:
-fortune_text= "big journeys begin with a single step."
+    fortune_text= "big journeys begin with a single step."
 elif fortune_number==4:
-fortune_text= "a fresh start will put everything in perspective."
+    fortune_text= "a fresh start will put everything in perspective."
 elif fortune_number==5:
-fortune_text="your hard work will soon pay off in unexpected ways."
-elif fortune_number=6:
-fortune_text="expect a pleasant surprise in the near future."
+    fortune_text="your hard work will soon pay off in unexpected ways."
+elif fortune_number==6:
+    fortune_text="expect a pleasant surprise in the near future."
 elif fortune_number==7:
-fortune_text= "your kindness will lead you to great success."
+    fortune_text= "your kindness will lead you to great success."
 elif fortune_number==8:
-fortune_Text= "an exciting opportunity is just around the corner."
+    fortune_Text= "an exciting opportunity is just around the corner."
 else:
-fortune_text= "error: the cookie is empty!"
+    fortune_text= "error: the cookie is empty!"
 print("CRACK! Your fortune cookie says:")
 print(f"\"{fortune_text}\"")
 print(f"your lucky number for today is: {lucky_number}")
